ce5bc08cefe15ff1c7e7e2693ea70f4cae7a99ff
Loyzerg <vorobey275@mail.ru>
2023-10-10 19:20:54 +0400

    Do lab

---
+++ Cruiser/Cruiser/Cruiser.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Cruiser
+{
+    partial class Cruiser
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxCruiser = new System.Windows.Forms.PictureBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCruiser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxCruiser
+            // 
+            this.pictureBoxCruiser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxCruiser.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxCruiser.Name = "pictureBoxCruiser";
+            this.pictureBoxCruiser.Size = new System.Drawing.Size(884, 461);
+            this.pictureBoxCruiser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxCruiser.TabIndex = 0;
+            this.pictureBoxCruiser.TabStop = false;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonCreate.Location = new System.Drawing.Point(12, 426);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = global::Cruiser.Properties.Resources.left;
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(629, 392);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = global::Cruiser.Properties.Resources.right;
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(791, 392);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 23);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = global::Cruiser.Properties.Resources.up;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(710, 363);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonUp.TabIndex = 4;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = global::Cruiser.Properties.Resources.down1;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(710, 392);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonDown.TabIndex = 5;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // Cruiser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.pictureBoxCruiser);
+            this.Name = "Cruiser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cruiser";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCruiser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxCruiser;
+        private Button buttonCreate;
+        private Button buttonLeft;
+        private Button buttonRight;
+        private Button buttonUp;
+        private Button buttonDown;
+    }
+}
---
+++ Cruiser/Cruiser/DrawingCruiser.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cruiser
+{
+    /// <summary>
+    /// Класс, отвечающий за прорисовку и перемещение объекта-сущности
+    /// </summary>
+    public class DrawningCruiser
+    {
+        /// <summary>
+        /// Класс-сущность
+        /// </summary>
+        public EntityCruiser? EntityCruiser { get; private set; }
+        /// <summary>
+        /// Ширина окна
+        /// </summary>
+        private int _pictureWidth;
+        /// <summary>
+        /// Высота окна
+        /// </summary>
+        private int _pictureHeight;
+        /// <summary>
+        /// Левая координата прорисовки Крейсера
+        /// </summary>
+        private int _startPosX;
+        /// <summary>
+        /// Верхняя кооридната прорисовки Крейсера
+        /// </summary>
+        private int _startPosY;
+        /// <summary>
+        /// Ширина прорисовки Крейсера
+        /// </summary>
+        private readonly int _cruiserWidth = 120;
+        /// <summary>
+        /// Высота прорисовки Крейсера
+        /// </summary>
+        private readonly int _cruiserHeight = 40;
+        /// <summary>
+        /// Инициализация свойств
+        /// </summary>
+        /// <param name="speed">Скорость</param>
+        /// <param name="weight">Вес</param>
+        /// <param name="bodyColor">Цвет кузова</param>
+        /// <param name="additionalColor">Дополнительный цвет</param>
+        /// <param name="width">Ширина картинки</param>
+        /// <param name="height">Высота картинки</param>
+        /// <returns>true - объект создан, false - проверка не пройдена, нельзя создать объект в этих размерах</returns>
+        public bool Init(int speed, double weight, Color bodyColor, Color additionalColor, int width, int height)
+        {
+            if (_cruiserWidth > width || _cruiserHeight > height) return false;
+            _pictureWidth = width;
+            _pictureHeight = height;
+            EntityCruiser = new EntityCruiser();
+            EntityCruiser.Init(speed, weight, bodyColor, additionalColor);
+            return true;
+        }
+        /// <summary>
+        /// Установка позиции
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        public void SetPosition(int x, int y)
+        {
+            if (x > _pictureWidth - _cruiserWidth || y > _pictureHeight - _cruiserHeight)
+            {
+                x = 0;
+                y = 0;
+            }
+            _startPosX = x;
+            _startPosY = y;
+        }
+        /// <summary>
+        /// Изменение направления перемещения
+        /// </summary>
+        /// <param name="direction">Направление</param>
+        public void MoveTransport(Direction direction)
+        {
+            if (EntityCruiser == null)
+        {
+                return;
+            }
+            switch (direction)
+            {
+                //влево
+                case Direction.Left:
+                    if (_startPosX - EntityCruiser.Step > 0)
+                    {
+                        _startPosX -= (int)EntityCruiser.Step;
+                    }
+                    break;
+                //вверх
+                case Direction.Up:
+                    if (_startPosY - EntityCruiser.Step > 0)
+                    {
+                        _startPosY -= (int)EntityCruiser.Step;
+                    }
+                    break;
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + EntityCruiser.Step < _pictureWidth - _cruiserWidth)
+                    {
+                        _startPosX += (int)EntityCruiser.Step;
+                    }
+                    break;
+                //вниз
+                case Direction.Down:
+                    if (_startPosY + EntityCruiser.Step < _pictureHeight - _cruiserHeight)
+                    {
+                        _startPosY += (int)EntityCruiser.Step;
+                    }
+                    break;
+            }
+        }
+        /// <summary>
+        /// Прорисовка объекта
+        /// </summary>
+        /// <param name="g"></param>
+        public void DrawTransport(Graphics g)
+        {
+            if (EntityCruiser == null)
+            {
+                return;
+            }
+            Pen pen = new(Color.Black);
+            Brush additionalBrush = new
+            SolidBrush(EntityCruiser.AdditionalColor);
+
+            //границы крейсера
+            Point point1 = new Point(_startPosX + 3, _startPosY);
+            Point point2 = new Point(_startPosX + 80, _startPosY);
+            Point point3 = new Point(_startPosX + 120, _startPosY + 20);
+            Point point4 = new Point(_startPosX + 80, _startPosY + 40);
+            Point point5 = new Point(_startPosX + 3, _startPosY + 40);
+
+            Point[] points =
+            {
+                point1, point2, point3, point4, point5
+            };
+            Brush br = new SolidBrush(EntityCruiser.BodyColor);
+            g.FillPolygon(br, points);
+            g.DrawPolygon(pen, points);
+            // турбины
+            Brush brBlack = new SolidBrush(Color.Black);
+
+            g.FillRectangle(brBlack, _startPosX, _startPosY + 10, 3, 10);
+            g.FillRectangle(brBlack, _startPosX, _startPosY + 21, 3, 10);
+
+            //блоки на палубе
+            Brush brBrown = new SolidBrush(Color.Brown);
+            g.FillRectangle(additionalBrush, _startPosX + 30, _startPosY + 12, 30, 15);
+            g.DrawRectangle(pen, _startPosX + 30, _startPosY + 12, 30, 15);
+
+            g.FillRectangle(additionalBrush, _startPosX + 60, _startPosY + 5, 15, 30);
+            g.DrawRectangle(pen, _startPosX + 60, _startPosY + 5, 15, 30);
+
+            g.FillEllipse(brBrown, _startPosX + 80, _startPosY + 12, 15, 15);
+            g.DrawEllipse(pen, _startPosX + 80, _startPosY + 12, 15, 15);
+        }
+    }
+}
